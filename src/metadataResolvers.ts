671b0b78619f332b4f25b4c99d6cdb7375e5010e
Charlie Jonas <charlie@callawaycloudconsulting.com>
2019-09-18 21:35:24 -0600

    added comments to md resolver

--- src/metadataResolvers.ts
+++ src/metadataResolvers.ts
@@ -1,7 +1,7 @@
 import { extname } from 'path';
 import { getFiles } from './util';
 
-// these need to be rewitten for windows... maybe use globs instead
+// these need to be re-witten for windows... maybe use globs instead
 const AURA_REGEX = /(.*\/aura\/\w*)\/.*/;
 const COMP_META = /.*(.cls|\.trigger|\.page|\.component)-meta.xml/;
 const STATIC_RESOURCE_FOLDER_REGEX = /(.*\/staticresources\/\w*)\/.*/;
@@ -14,7 +14,7 @@ interface MetadataResolver {
 
 // order from most selective to least
 const metadataResolvers: MetadataResolver[] = [
-  {
+  { // MD that has paired -meta.xml file.  Probably missing some types
     match: path => {
       return ['.cls', '.trigger', '.page', '.component'].includes(extname(path));
     },
@@ -22,13 +22,13 @@ const metadataResolvers: MetadataResolver[] = [
       return [path, path + '-meta.xml'];
     }
   },
-  {
+  { // reverse of above rule
     match: COMP_META,
     getMetadataPaths: async (path: string) => {
       return [path, path.replace('-meta.xml', '')];
     }
   },
-  {
+  { // other metadata
     match: path => {
       return path.endsWith('-meta.xml');
     },
@@ -36,21 +36,21 @@ const metadataResolvers: MetadataResolver[] = [
       return [path];
     }
   },
-  {
+  { // aura bundles
     match: AURA_REGEX,
     getMetadataPaths: async (path: string) => {
       const appDir = AURA_REGEX.exec(path)[1];
       return await getFiles(appDir);
     }
   },
-  {
+  { // decompressed static resource (folders)
     match: STATIC_RESOURCE_FOLDER_REGEX,
     getMetadataPaths: async (path: string) => {
       const appDir = STATIC_RESOURCE_FOLDER_REGEX.exec(path)[1];
       return [...await getFiles(appDir), `${appDir}.resource-meta.xml`];
     }
   },
-  {
+  { // static resource (single files)
     match: STATIC_RESOURCE_FILE_REGEX,
     getMetadataPaths: async (path: string) => {
       const baseName = STATIC_RESOURCE_FILE_REGEX.exec(path)[1];
@@ -59,6 +59,7 @@ const metadataResolvers: MetadataResolver[] = [
   }
 ];
 
+// given a path, return all other paths that must be included along side for a valid deployment
 export function resolveMetadata(path: string) {
   for (const resolver of metadataResolvers) {
     const isMatch = resolver.match instanceof RegExp ? resolver.match.test(path) : resolver.match(path);
