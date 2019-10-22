0dc5ef88c3ed18c3a1417e96b34cdc4bb9c0698d
Ralph Callaway <ralph@callawaycloudconsulting.com>
2019-10-22 11:08:23 -0600

    very basic test

--- test/commands/hello/org.test.ts
+++ test/commands/hello/org.test.ts
@@ -1,19 +1,38 @@
-import { expect, test } from '@salesforce/command/lib/test';
-import { ensureJsonMap, ensureString } from '@salesforce/ts-types';
+import * as fs from 'fs';
+import { exec } from 'child_process';
+import * as assert from 'assert';
 
-describe('hello:org', () => {
-  test
-    .withOrg({ username: 'test@org.com' }, true)
-    .withConnectionRequest(request => {
-      const requestMap = ensureJsonMap(request);
-      if (ensureString(requestMap.url).match(/Organization/)) {
-        return Promise.resolve({ records: [ { Name: 'Super Awesome Org', TrialExpirationDate: '2018-03-20T23:24:11.000+0000'}] });
-      }
-      return Promise.resolve({ records: [] });
-    })
-    .stdout()
-    .command(['hello:org', '--targetusername', 'test@org.com'])
-    .it('runs hello:org --targetusername test@org.com', ctx => {
-      expect(ctx.stdout).to.contain('Hello world! This is org: Super Awesome Org and I will be around until Tue Mar 20 2018!');
+async function myExec(cmd:string) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(), 10000);
+    exec(cmd, (err, stdout, stderr) => {
+      resolve({err, stdout, stderr});
     });
+  });
+}
+
+function turnGitOn(dir: string) {
+  process.chdir(dir);
+  fs.renameSync('.notgit','.git');
+}
+
+function turnGitOff() {
+  fs.renameSync('.git','.notgit');
+}
+const proj_path = "test/projects/basic_change";
+describe('git:package', () => {
+  before(() => {
+    turnGitOn(proj_path);
+  });
+  it('it builds a deployment with changed files', async () => {
+    try {
+      const res = await myExec('sfdx git:package -d deploy --purge');
+      assert.ok(fs.existsSync("deploy/package.xml"));
+    } catch (e) {
+      assert.fail(e);
+    }
+  });
+  after(() => {
+    turnGitOff();
+  });
 });
